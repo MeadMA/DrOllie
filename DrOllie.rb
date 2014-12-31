a10143a09e8aa5a49949f732e4565aa1cd14a86c
Michael A. Mead <michael.a.mead@gmail.com>
2014-12-30 19:51:50 -0500

    Initial commit

---
+++ DrOllie.rb
@@ -0,0 +1,45 @@
+load 'support.rb'
+
+# Warn and exit if user is not an administrator or UAC-elevated.
+unless is_admin?
+	print "ERROR: Dr. Ollie must be run with administrative credentials.\nEnsure the user is an administrator and UAC-elevated.\n"
+	exit(1)
+end
+
+# Get architecture.  If 64-bit, get 64-bit software and 32-bit software (from
+# WOW6432Node).
+if RUBY_PLATFORM == 'x64-mingw32'
+	software32 = query_uninstall(true)
+	software64 = query_uninstall
+else
+	software32 = query_uninstall
+	software64 = []
+end
+
+# Read in definitions from .CONF files in 'defs' directory.
+defs = read_defs
+
+# Run through each definition and run the check configured.
+defs.each do |d|
+	log_event(6, 'INFORMATION', "Running check for #{d["description"]}")
+	# Read architecture defined for this check and grab applicable software for
+	# check.
+	if d["arch"] == "64"
+		software = software64
+	else
+		software = software32
+	end
+	# Perform check based on the "method" defined
+	case d["method"]
+	when "installed_program"
+		if installed_program_outdated?(d, software)
+			init_update(d)
+		end
+	when "registry_version"
+		if registry_version_outdated?(d)
+			init_update(d)
+		end
+	else
+		log_event(8, 'ERROR', "The update method specified ('#{d["method"]}') in the definition for '#{d["description"]}' is invalid.")
+	end
+end
---
+++ support.rb
@@ -0,0 +1,287 @@
+require 'win32/registry'
+require 'json'
+require 'open-uri'
+
+# Query the 'uninstall' key from the Registry to get a list of installed
+# software.  Return an array of arrays.  Each member array is the name of the
+# software and its version.
+# If "true" is passed to this method, query from Wow6432Node (32-bit software
+# installed on 64-bit system)
+def query_uninstall(wow64=false)
+	if wow64
+		key = 'Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall'
+	else
+		key = 'Software\Microsoft\Windows\CurrentVersion\Uninstall'
+	end
+	ary = []
+	# Iterate through each key under Uninstall
+	Win32::Registry::HKEY_LOCAL_MACHINE.open(key) do |reg|
+		reg.each_key do |key1,key2|
+			k = reg.open(key1)
+			name = ''
+			version = ''
+			# Get the DisplayName value; use rescue in case the value doesn't
+			# exist
+			begin
+				name = k["DisplayName"]
+			rescue
+			end
+			# Get the DisplayVersion value; use rescue in case the vaue doesn't
+			# exit
+			begin
+				version = k["DisplayVersion"]
+			rescue
+			end
+			ary << [name, version]
+		end
+	end
+	# Delete entries where the DisplayName value was blank or didn't exist
+	ary.delete_if do |n, v|
+		n == ''
+	end
+	# Return the collected software
+	return ary
+end
+
+# Read the .CONF JSON files from the 'def' directory and return an array.
+# Each entry in the array is a hash representing a definition.
+def read_defs
+	defs = Array.new
+	# Get list of .CONF files from 'def' directory
+	def_files = Dir["./def/*.conf"]
+	# Read each file and add it to the array
+	def_files.each do |file|
+		f = File.read(file)
+		d = JSON.parse(f)
+		# Convert the "name" string to a regular expression
+		d["name"] = Regexp.new d["name"]
+		# Convert the "success_codes" string to an array
+		d["success_codes"] = d["success_codes"].split(',')
+		defs << d
+	end
+	# Return the array of definitions
+	return defs
+end
+
+# "installed_program" method
+# Given a definition and an array of installed software (collected using
+# "query_uninstall" method), check if software is outdated
+# Returns 'true' if outdated and 'false' if not
+def installed_program_outdated?(def_hsh, software)
+	# Iterate through each item of installed software
+	software.each do |name, version|
+		# If the software's name matches the regular expression defined in the
+		# definition, then compare versions
+		if name =~ def_hsh["name"]
+			# Use "version_less_than?" method to see if installed software's
+			# version is less than the version from the definition
+			if version_less_than?(version, def_hsh["min_version"])
+				log_event(7, 'INFORMATION', "#{def_hsh["description"]} is out-of-date.")
+				return true
+			end
+		end
+	end
+	return false
+end
+
+# "registry_version" method
+# Given a definition, grab a version from the Registry value specified
+# Return true if outdated, false if not
+def registry_version_outdated?(def_hsh)
+	case def_hsh["registry_hive"]
+	when "HKLM", "HKEY_LOCAL_MACHINE"
+		keycmd = reg_hklm
+	when "HKCU", "HKEY_CURRENT_USERS"
+		keycmd = reg_hkcu
+	when "HKU", "HKEY_USERS"
+		keycmd = reg_hku
+	when "HKCC", "HKEY_CURRENT_CONFIG"
+		keycmd = reg_hkcc
+	when "HKCR", "HKEY_CLASSES_ROOT"
+		keycmd = reg_hkcr
+	else
+		log_event(9, 'ERROR', "The registry hive specified ('#{def_hsh["registry_hive"]}' in the definition for '#{def_hsh["description"]}' is invalid.")
+		return false
+	end
+	
+	reg_version = send(keycmd, def_hsh["registry_key"], def_hsh["registry_value"])
+	unless reg_version
+		return false
+	end
+	if version_less_than?(reg_version, def_hsh["min_version"])
+		log_event(7, 'INFORMATION', "#{def_hsh["description"]} is out-of-date.")
+		return true
+	end
+	return false
+end
+
+# Given a regular expression and an array of installed software, see if any of
+# the installed software matches the expression
+# Return 'true' if installed or 'false' if not
+def is_installed?(name_regexp, software)
+	software.each do |name, version|
+		if name =~ name_regexp
+			return true
+		end
+	end
+	return false
+end
+
+# Downloads from a specified URL to a specified destination
+def download_update(url, dest)
+	if File.exist?(dest)
+		log_event(2, 'INFORMATION', "Download of '#{url}' to '#{dest}' cancelled. '#{dest}' already exists.")
+	else
+		log_event(1, 'INFORMATION', "Downloading '#{url}' to '#{dest}'.")
+		open(dest, 'wb') do |file|
+			file << open(url).read
+		end
+	end
+end
+
+# Given two versions, see if the first version is smaller/older than the second
+# version
+# Return true if smaller/older, false if not
+def version_less_than?(ver1, ver2)
+	# Some sick manufacturers use commas instead of periods in their versions
+	# Convert these commas to periods for the comparison
+	ver1 = ver1.gsub(',', '.')
+	ver2 = ver2.gsub(',', '.')
+	# Split the versions at each period
+	ver1 = ver1.split('.')
+	ver2 = ver2.split('.')
+	
+	# For each level, see if the first version is less than the second version
+	# Example, 5.0.6.4 and 5.0.6.8 will get caught on the fourth level
+	for i in 0..9
+		if ver1[i].to_i < ver2[i].to_i
+			return true
+		end
+	end
+	return false
+end
+
+# Use 'eventcreate' command to log an event in the Application log, using the
+# source name "DrOllie".  Use the provided ID, event type (SUCCESS, ERROR,
+# WARNING, or INFORMATION), and description.
+# Also print the event to the command line
+def log_event(id, type, desc)
+	`eventcreate /l APPLICATION /t #{type.upcase} /so DrOllie /id #{id.to_s} /d "#{desc}"`
+	print "#{type}: #{desc}\n"
+end
+
+# Reads a given key/value from HKLM
+def reg_hklm(key, value)
+	begin
+		Win32::Registry::HKEY_LOCAL_MACHINE.open(key) do |reg|
+			return reg[value]
+		end
+	rescue
+		return false
+	end
+end
+
+# Reads a given key/value from HKCU
+def reg_hkcu(key, value)
+	begin
+		Win32::Registry::HKEY_CURRENT_USER.open(key) do |reg|
+			return reg[value]
+		end
+	rescue
+		return false
+	end
+end
+
+# Reads a given key/value from HKU
+def reg_hku(key, value)
+	begin
+		Win32::Registry::HKEY_USERS.open(key) do |reg|
+			return reg[value]
+		end
+	rescue
+		return false
+	end
+end
+
+# Reads a given key/value from HKCC
+def reg_hkcc(key, value)
+	begin
+		Win32::Registry::HKEY_CURRENT_CONFIG.open(key) do |reg|
+			return reg[value]
+		end
+	rescue
+		return false
+	end
+end
+
+# Reads a given key/value from HKCR
+def reg_hkcr(key, value)
+	begin
+		Win32::Registry::HKEY_CLASSES_ROOT.open(key) do |reg|
+			return reg[value]
+		end
+	rescue
+		return false
+	end
+end
+
+# Attempts to query the user Registry hive for the local SYSTEM account. If it
+# fails, the user is either not an administrator or is not UAC-elevated.
+# Returns true if an admin or false if not.
+def is_admin?
+	is_admin = false
+	begin
+		Win32::Registry::HKEY_USERS.open('S-1-5-19') { |reg| }
+		is_admin = true
+	rescue
+	end
+	return is_admin
+end
+
+# Perform the update action specified by the definition
+# Print output to a log in the "output" directory
+def update_action(def_hsh)
+	# Set command based on action specified in the definition
+	case def_hsh["action"].downcase
+	when "execute_downloaded_file"
+		if def_hsh["action_parameters"]
+			cmd = "\"./update_files/#{def_hsh["file_name"]}\" #{def_hsh["action_parameters"]}"
+		else
+			cmd = "\"./update_files/#{def_hsh["file_name"]}\""
+		end
+	end
+	
+	# Execute the command determined above
+	log_event(3, 'INFORMATION', "Executing update action for #{def_hsh["description"]} (#{cmd})")
+	output = `#{cmd}`
+	exit_code=$?.exitstatus.to_s
+	
+	# Log output from command; convert line feeds to Windows style (yuck)
+	log_file = "./output/#{def_hsh["action"]}_#{Time.now.strftime("%Y%m%d_%H%M%S")}.log"
+	output = output.gsub("\r\n", "\n").gsub("\n", "\r\n")
+	File.open(log_file, 'w') do |file|
+		file.puts output
+	end
+	
+	# Check exit code against successful codes from the definition
+	# If succeeded, delete the update file
+	if def_hsh["success_codes"].include?(exit_code)
+		log_event(4, 'SUCCESS', "Update action for #{def_hsh["description"]} succeeded with exit code #{exit_code}.")
+		File.delete("./update_files/#{def_hsh["file_name"]}")
+	else
+		log_event(5, 'WARNING', "Update action for #{def_hsh["description"]} failed with exit code #{exit_code}.")
+	end
+end
+
+# Initiates the update sequence for a given definition
+def init_update(def_hsh)
+	# Get update URL from definition
+	url = def_hsh["download_url"]
+	# Download to 'update_files' directory and name the file based on
+	# the definition
+	dest = "./update_files/" + def_hsh["file_name"]
+	# Perform the download
+	download_update(url, dest)
+	# Execute update action
+	update_action(def_hsh)
+end
